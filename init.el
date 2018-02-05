8eedb9d1bc0543324940abc43c0141c19fec732d
David Castro-Perez <david.castro.dcp@gmail.com>
2018-02-05 12:10:57 +0000

    Cleanup

--- init.el
+++ init.el
@@ -2,17 +2,3 @@
 (load "~/.emacs.d/load-directory")
 (load "~/.emacs.d/use-package")
 (load-directory "~/.emacs.d/config")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (## latex-pretty-symbols yasnippet yaml-mode window-number utop use-package tuareg solarized-theme merlin markdown-mode magit ghci-completion fill-column-indicator evil company-quickhelp company-math company-ghci company-ghc company-cabal))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
