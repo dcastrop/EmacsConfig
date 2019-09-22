bac68ec57678dbf1d81b77e078c52a9b227707f2
David Castro-Perez <david.castro.dcp@gmail.com>
2019-09-22 15:53:14 +0100

    Updates

--- use-package.el
+++ use-package.el
@@ -1,9 +1,11 @@
 (require 'package)
+;; (setq package-check-signature nil)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.milkbox.net/packages/"))
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (package-initialize)
+(package-refresh-contents)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
